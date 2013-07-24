c484fbf76030c690d090ce365e275de04ea5bf48
Arnav Gupta <championswimmer@aokp.co>
2013-07-25 01:14:34 +0530

    Initial commit

---
+++ esya/src/main/java/in/ac/iiitd/esya/HomeScreenActivity.java
@@ -0,0 +1,23 @@
+package in.ac.iiitd.esya;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class HomeScreenActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homescreen);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home_screen, menu);
+        return true;
+    }
+    
+}
